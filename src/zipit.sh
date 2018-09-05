3f7dbd70c1188d4b12be6efec2407475404a7db6
James Shull <dwjshull@dwam.densocorp.com>
2018-09-05 16:05:21 -0700

    add automated s3 upload to postbuild. Change color of ammort. button

--- src/zipit.sh
+++ src/zipit.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+# Zip it
 gzip -9 ../dist/rentcheck/runtime.*.js
 gzip -9 ../dist/rentcheck/main.*.js
 gzip -9 ../dist/rentcheck/polyfills.*.js
 gzip -9 ../dist/rentcheck/styles.*.css
 
+# Rename for AWS
 for f in ../dist/rentcheck/*.gz; do mv "$f" "${f%.gz}"; done
+
+# Send to AWS
+aws s3 cp --include "*.js" --include "*.css" --include "*.html"
