523af7d96444a14ae290b41c74bcac616ce397f4
James Shull <dwjshull@dwam.densocorp.com>
2018-09-06 13:32:43 -0700

    tested zipit.bat

--- src/zipit.bat
+++ src/zipit.bat
@@ -1,15 +1,22 @@
 @echo off
 echo "postbuild batch file would normally run now"
-
 REM Zip javascript and css
-REM 7z a -tgzip -mx=9 "dist\rentcheck\main.js.gz" "dist\rentcheck\main.*.js"
-REM 7z a -tgzip -mx=9 "dist\rentcheck\polyfills.js.gz" "dist\rentcheck\polyfills.*.js"
-REM 7z a -tgzip -mx=9 "dist\rentcheck\runtime.js.gz" "dist\rentcheck\runtime.*.js"
-REM 7z a -tgzip -mx=9 "dist\rentcheck\styles.css.gz" "dist\rentcheck\styles.*.css"
+CD dist\rentcheck
+FOR %%i IN (*.js) DO (
+  7z.exe a -tgzip -mx=9 "%%~ni.js.gz" "%%i"
+  DEL "%%i"
+  MOVE "%%i.gz" "%%~ni.js"
+)
+FOR %%i IN (*.css) DO (
+  7z.exe a -tgzip -mx=9 "%%~ni.css.gz" "%%i"
+  DEL "%%i"
+  MOVE "%%i.gz" "%%~ni.css"
+)
+CD ..\..
 
-REM Rename to original file name
+REM Rename to original file name (in above loop)
 
 REM Send to AWS
-REM aws s3 cp dist\rentcheck s3://rentcheck.ninja --recursive --exclude "*" --include "*.js" --content-type "text/javascript" --content-encoding "gzip" --acl public-read
-REM aws s3 cp dist\rentcheck s3://rentcheck.ninja --recursive --exclude "*" --include "*.css" --content-type "text/css" --content-encoding "gzip" --acl public-read
-REM aws s3 cp dist\rentcheck s3://rentcheck.ninja --recursive --exclude "*" --include "*.html" --acl public-read
+aws s3 cp dist\rentcheck s3://rentcheck.ninja --recursive --exclude "*" --include "*.js" --content-type "text/javascript" --content-encoding "gzip" --acl public-read
+aws s3 cp dist\rentcheck s3://rentcheck.ninja --recursive --exclude "*" --include "*.css" --content-type "text/css" --content-encoding "gzip" --acl public-read
+aws s3 cp dist\rentcheck s3://rentcheck.ninja --recursive --exclude "*" --include "*.html" --acl public-read
